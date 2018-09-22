b8847d5ca6c74fccb4855c752315d094479d9b33
Dean-Chu <zhudi_bupt@163.com>
2018-09-22 22:43:20 +0800

    adjust cpu freq

--- include/generated/timestamp_autogenerated.h
+++ include/generated/timestamp_autogenerated.h
@@ -1,4 +1,4 @@
-#define U_BOOT_DATE "Sep 16 2018"
-#define U_BOOT_TIME "01:02:31"
+#define U_BOOT_DATE "Sep 20 2018"
+#define U_BOOT_TIME "00:16:22"
 #define U_BOOT_TZ "+0800"
-#define U_BOOT_DMI_DATE "09/16/2018"
+#define U_BOOT_DMI_DATE "09/20/2018"
--- include/generated/version_autogenerated.h
+++ include/generated/version_autogenerated.h
@@ -1,4 +1,4 @@
-#define PLAIN_VERSION "2017.01-rc2-gd97bf69-dirty"
+#define PLAIN_VERSION "2017.01-rc2-gf6a8cb7-dirty"
 #define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
 #define CC_VERSION_STRING "arm-linux-gnueabihf-gcc (Linaro GCC 6.3-2017.05) 6.3.1 20170404"
 #define LD_VERSION_STRING "GNU ld (Linaro_Binutils-2017.05) 2.27.0.20161019"
