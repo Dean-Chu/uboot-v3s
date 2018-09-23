24e0a08d1b3558e5251c455f445cbd8f8fcf70f7
Dean-Chu <zhudi_bupt@163.com>
2018-09-23 19:49:29 +0800

    modified st7789v gamma settings

--- include/generated/timestamp_autogenerated.h
+++ include/generated/timestamp_autogenerated.h
@@ -1,4 +1,4 @@
-#define U_BOOT_DATE "Sep 20 2018"
-#define U_BOOT_TIME "00:16:22"
+#define U_BOOT_DATE "Sep 23 2018"
+#define U_BOOT_TIME "19:46:59"
 #define U_BOOT_TZ "+0800"
-#define U_BOOT_DMI_DATE "09/20/2018"
+#define U_BOOT_DMI_DATE "09/23/2018"
